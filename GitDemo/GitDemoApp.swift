ed99c360fecf46fd293ee19a5c91c692cc34fcbe
nien <nien@linebreak.studio>
2023-11-02 17:20:58 -0400

    My first checkin.

---
+++ GitDemo/GitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDemoApp.swift
+//  GitDemo
+//
+//  Created by Nien Lam on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
